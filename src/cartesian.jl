09380cc987c439bcc95c86aa711f7c7eaaa56e6c
Moelf <proton@jling.dev>
2022-07-28 15:06:33 -0400

    add fromPxPyPzM

--- src/cartesian.jl
+++ src/cartesian.jl
@@ -1,9 +1,9 @@
 Base.zero(lv::T) where T<:LorentzVector = T(0,0,0,0)
 mass(lv::LorentzVector) = sqrt(dot(lv, lv))
-pt(lv::LorentzVector) = sqrt(lv.x^2 + lv.y^2)
+pt(lv::LorentzVector) = sqrt(muladd(lv.x, lv.x, lv.y^2))
 mt2(lv::LorentzVector) = lv.t^2 - lv.z^2
 mt(lv::LorentzVector) = mt2(lv)<0 ? -sqrt(-mt2(lv)) : sqrt(mt2(lv))
-mag(lv::LorentzVector) = sqrt(lv.x^2 + lv.y^2 + lv.z^2)
+mag(lv::LorentzVector) = sqrt(muladd(lv.x, lv.x, lv.y^2) + lv.z^2)
 energy(lv::LorentzVector) = lv.t
 px(lv::LorentzVector) = lv.x
 py(lv::LorentzVector) = lv.y
@@ -48,4 +48,9 @@ function deltar(lv1, lv2)
     deta = eta(lv1) - eta(lv2)
     dphi = deltaphi(lv1 - lv2)
     return sqrt(fma(deta, deta, dphi * dphi))
+end
+
+function fromPxPyPzM(px, py, pz, m)
+    e = sqrt(muladd(px, px, py^2) + muladd(pz, pz, m^2))
+    return LorentzVector(e, px, py, pz)
 end

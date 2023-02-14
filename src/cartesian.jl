4c8df0257c810aae0a1d602c632a20f5bf132711
Jerry Ling <proton@jling.dev>
2023-02-14 17:13:41 -0500

    Update cartesian.jl

--- src/cartesian.jl
+++ src/cartesian.jl
@@ -46,7 +46,7 @@ deltaphi(lv1, lv2) = phi_mpi_pi(phi(lv1) - phi(lv2))
 
 function deltar(lv1, lv2)
     deta = eta(lv1) - eta(lv2)
-    dphi = deltaphi(lv1 - lv2)
+    dphi = deltaphi(lv1, lv2)
     return sqrt(fma(deta, deta, dphi * dphi))
 end
 
